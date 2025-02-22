adeba616b45e8c43efbae933d918cc8c93bdbf57
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-22 07:54:56 +0100

    installed scss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<body> </body>
+
+<style lang="scss">
+	body {
+		background-color: black;
+	}
+</style>
+
