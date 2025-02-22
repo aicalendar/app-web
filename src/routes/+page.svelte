1d2efb7acc630092be7429bbe0f88dd7983fde9c
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-22 09:07:54 +0100

    box with 2 sides

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,34 @@
-<body> </body>
+<div class="main">
+	<div class="wrapper">
+		<div class="left-side">D</div>
+		<div class="right-side">Ddada</div>
+	</div>
+</div>
 
 <style lang="scss">
-	body {
-		background-color: black;
+	.main {
+		margin: 0;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		.wrapper {
+			display: flex;
+			width: 60%;
+			height: 420px;
+			.left-side {
+				background-color: #09090b;
+				width: 50%;
+				height: 100%;
+				border-radius: 12px 0px 0px 12px;
+			}
+			.right-side {
+				background-color: #18181b;
+				width: 50%;
+				height: 100%;
+				border-radius: 0px 12px 12px 0px;
+			}
+		}
 	}
 </style>
-
