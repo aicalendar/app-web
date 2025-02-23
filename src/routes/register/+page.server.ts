bd94a4b31a30048aaeeb7e73886cf66d75d8c185
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-23 09:54:57 +0100

    npm run format

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,43 +1,43 @@
 import { fail, type Actions } from '@sveltejs/kit';
 
 interface RegisterResponse {
-  status: string;
-  error?: string;
-  sessionToken: {
-    token: string;
-    expiration: number;
-  };
-  user: {
-    id: string;
-    email: string;
-    password: string;
-    salt: string;
-    createdAt: string;
-  };
+	status: string;
+	error?: string;
+	sessionToken: {
+		token: string;
+		expiration: number;
+	};
+	user: {
+		id: string;
+		email: string;
+		password: string;
+		salt: string;
+		createdAt: string;
+	};
 }
 
 export const actions = {
-  register: async ({ request }) => {
-    const data = await request.formData();
-    const email = data.get('email');
-    const password = data.get('password');
+	register: async ({ request }) => {
+		const data = await request.formData();
+		const email = data.get('email');
+		const password = data.get('password');
 
-    const response = await fetch('http://api-user-service/users/register', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: email,
-        password: password
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const responseData: RegisterResponse = await response.json();
+		const response = await fetch('http://api-user-service/users/register', {
+			method: 'POST',
+			body: JSON.stringify({
+				email: email,
+				password: password
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const responseData: RegisterResponse = await response.json();
 
-    if (responseData.error != null) {
-      return fail(response.status, { error: responseData.error });
-    }
+		if (responseData.error != null) {
+			return fail(response.status, { error: responseData.error });
+		}
 
-    return { success: true }
-  }
+		return { success: true };
+	}
 } satisfies Actions;
