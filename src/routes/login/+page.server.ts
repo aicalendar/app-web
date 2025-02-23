ffc01966aff49c3754a85f65382b777f860f6aa5
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-23 06:39:12 +0100

    working login route (#21)
    * installed scss
    
    * box with 2 sides
    
    * upper text
    
    * form with username and password
    
    * login button
    
    * Signup button
    
    * hot reload svelte
    
    * set cookie after login with API
    
    * moved to login route
    
    * fix error not found
    
    * npm run format
    
    * empty href fix
    
    * errror message
    
    * npm run format

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,48 @@
+import { fail, type Actions } from '@sveltejs/kit';
+
+interface LoginResponse {
+	status: string;
+	error?: string;
+	sessionToken: {
+		token: string;
+		expiration: number;
+	};
+	user: {
+		id: string;
+		name: string;
+		password: string;
+		salt: string;
+		createdAt: string;
+	};
+}
+
+export const actions = {
+	login: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const username = data.get('username');
+		const password = data.get('password');
+
+		const response = await fetch('http://api-user-service/users/login', {
+			method: 'POST',
+			body: JSON.stringify({
+				name: username,
+				password: password
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const responseData: LoginResponse = await response.json();
+
+		if (responseData.error != null) {
+			return fail(response.status, { error: responseData.error });
+		}
+
+		cookies.set('sessionToken', responseData.sessionToken.token, {
+			maxAge: responseData.sessionToken.expiration,
+			secure: false,
+			path: '/',
+			httpOnly: true
+		});
+	}
+} satisfies Actions;
