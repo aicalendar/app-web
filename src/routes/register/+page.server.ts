6725e11718c1abe636c5d6bb003c26bfdab0e6ee
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-23 08:52:54 +0100

    register action form

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,41 @@
+import { fail, type Actions } from '@sveltejs/kit';
+
+interface RegisterResponse {
+  status: string;
+  error?: string;
+  sessionToken: {
+    token: string;
+    expiration: number;
+  };
+  user: {
+    id: string;
+    name: string;
+    password: string;
+    salt: string;
+    createdAt: string;
+  };
+}
+
+export const actions = {
+  register: async ({ request }) => {
+    const data = await request.formData();
+    const email = data.get('email');
+    const password = data.get('password');
+
+    const response = await fetch('http://api-user-service/users/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const responseData: RegisterResponse = await response.json();
+
+    if (responseData.error != null) {
+      return fail(response.status, { error: responseData.error });
+    }
+  }
+} satisfies Actions;
