0ee951253219ec9baec1f30b6a285813fdfef729
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-23 05:54:36 +0100

    npm run format

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,49 +1,49 @@
-import type { Actions } from "@sveltejs/kit";
+import type { Actions } from '@sveltejs/kit';
 
 interface LoginResponse {
-  status: string;
-  error?: string;
-  sessionToken: {
-    token: string;
-    expiration: number;
-  }
-  user: {
-    id: string;
-    name: string;
-    password: string;
-    salt: string;
-    createdAt: string;
-  }
+	status: string;
+	error?: string;
+	sessionToken: {
+		token: string;
+		expiration: number;
+	};
+	user: {
+		id: string;
+		name: string;
+		password: string;
+		salt: string;
+		createdAt: string;
+	};
 }
 
 export const actions = {
-  login: async ({ request, cookies }) => {
-    const data = await request.formData();
-    const username = data.get('username');
-    const password = data.get('password');
+	login: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const username = data.get('username');
+		const password = data.get('password');
 
-    const response = await fetch('http://api-user-service/users/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        name: username,
-        password: password
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const responseData: LoginResponse = await response.json()
+		const response = await fetch('http://api-user-service/users/login', {
+			method: 'POST',
+			body: JSON.stringify({
+				name: username,
+				password: password
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const responseData: LoginResponse = await response.json();
 
-    if (responseData.error != null) {
-      console.error(responseData.error)
-      return
-    }
+		if (responseData.error != null) {
+			console.error(responseData.error);
+			return;
+		}
 
-    cookies.set("sessionToken", responseData.sessionToken.token, {
-      maxAge: responseData.sessionToken.expiration,
-      secure: false,
-      path: "/",
-      httpOnly: true,
-    });
-  },
+		cookies.set('sessionToken', responseData.sessionToken.token, {
+			maxAge: responseData.sessionToken.expiration,
+			secure: false,
+			path: '/',
+			httpOnly: true
+		});
+	}
 } satisfies Actions;
